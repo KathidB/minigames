059d0a4a575998a406ec8eb1f2cc594c5c6df347
KathidB <Kathidbb@gmail.com>
2022-06-12 11:02:05 +0200

    fixed reset button

--- dist/js/gtn.min.js
+++ dist/js/gtn.min.js
@@ -4,14 +4,20 @@ const btnReset = document.querySelector(".btn-reset");
 const gtnStatus = document.querySelector(".gtn-status");
 const gtnResult = document.querySelector(".gtn-result");
 const gtnMoves = document.querySelector(".gtn-moves");
+let randomNumber;
 
-let randomNumber = Number((Math.random() * 100).toFixed(0));
+const generateRandomNumber = () => {
+  randomNumber = Number((Math.random() * 100).toFixed(0));
+};
+generateRandomNumber();
+
 let counter = 0;
 
 const checkTheNumber = () => {
   if (gtnInput.value < 0 || gtnInput.value > 100) {
     gtnStatus.textContent = "Podaj liczbę z przedziału 0 do 100";
     gtnStatus.style.color = "tomato";
+    gtnInput.value = "";
   } else if (gtnInput.value.length === 0) {
     gtnStatus.textContent = "Musisz podać liczbę.";
     gtnStatus.style.color = "tomato";
@@ -19,11 +25,15 @@ const checkTheNumber = () => {
     counter++;
     gtnMoves.textContent = `${counter} ruch`;
     if (gtnInput.value > randomNumber) {
-      gtnStatus.textContent = "Podano za duża liczbę.";
+      // gtnStatus.textContent = "Podano za duża liczbę.";
+
+      gtnStatus.textContent = `${gtnInput.value} - Podana liczba jest za duża.`;
       gtnStatus.style.color = "white";
+      gtnInput.value = "";
     } else if (gtnInput.value < randomNumber) {
-      gtnStatus.textContent = "Podano za małą liczbę.";
+      gtnStatus.textContent = `${gtnInput.value} - Podana liczba jest za mała.`;
       gtnStatus.style.color = "white";
+      gtnInput.value = "";
     } else {
       counter;
       gtnStatus.textContent = "";
@@ -38,9 +48,20 @@ const checkTheNumber = () => {
 };
 
 btnCheck.addEventListener("click", checkTheNumber);
+const enterKeyCheck = (e) => {
+  e.key === "Enter" ? checkTheNumber() : false;
+};
+gtnInput.addEventListener("keyup", enterKeyCheck);
 
-gtnInput.addEventListener("keydown", function (e) {
-  if (e.code === "Enter") {
-    checkTheNumber();
-  }
-});
+const restartTheGame = () => {
+  gtnStatus.textContent = "";
+  gtnMoves.textContent = "";
+  gtnResult.textContent = "";
+  gtnInput.value = "";
+  btnCheck.classList.toggle("hide");
+  btnReset.classList.toggle("active");
+  counter = 0;
+  generateRandomNumber();
+};
+
+btnReset.addEventListener("click", restartTheGame);
